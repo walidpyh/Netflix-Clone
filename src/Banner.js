42be94322b5bb05e3be8587a6fe1b2885c24dfc1
Pyhoma69 <Pyhoma69@protonmail.com>
2022-01-28 15:34:06 -0800

    Added TMDB API

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,25 @@
-import React from "react";
+import axios from "./axios";
+import requests from "./Requests";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+
+        fetchData();
+    }, []);
+
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n - 1) + "..." : string;
     }
@@ -11,17 +29,19 @@ function Banner() {
             style={{
                 backgroundSize: "cover",
                 backgroundPosition: "center center",
-                backgroundImage: `url(./assets/images/banner.jpg)`,
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             }}
         >
             <div className="banner_contents">
-                <h1 className="banner_title">IN FROM THE COLD</h1>
+                <h1 className="banner_title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
                 <div className="banner_buttons">
                     <button className="banner_button">Play</button>
                     <button className="banner_button">More Info</button>
                 </div>
                 <h1 className="banner_description">
-                    {truncate(`This is a test .`, 150)}
+                    {truncate(movie?.overview, 150)}
                 </h1>
             </div>
             <div className="banner--fadeBottom" />

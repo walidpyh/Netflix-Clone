17ce0dac86499db9cb8b043aa5d7254419af3d8a
Pyhoma69 <Pyhoma69@protonmail.com>
2022-01-28 12:45:36 -0800

    gitignore additions

---
+++ src/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomeScreen.css'
+import Nav from './Nav';
+
+function HomeScreen() {
+  return <div className='homeScreen'>
+      <Nav />
+  </div>;
+}
+
+export default HomeScreen;
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css'
+
+function Nav() {
+  return <div className='nav'>
+      <img src="/assets/images/logo.png" alt="" />
+
+      <img src="/assets/images/profile.png" alt="" />
+  </div>;
+}
+
+export default Nav;

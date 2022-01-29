3b400dd11428ec7b4d38dadac0f1af0ac76c2004
Pyhoma69 <Pyhoma69@protonmail.com>
2022-01-29 08:31:47 -0800

    Added Firebase login and register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { auth } from "./firebase";
 
 function App() {
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+            if (userAuth) {
+                //logged
+            } else {
+                //logged out
+            }
+        });
+        return unsubscribe;
+    }, []);
     return (
         <div className="app">
             <BrowserRouter>
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
+import { auth } from "../firebase";
 import "./SignupScreen.css";
 
 function SignUpScreen() {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
     const register = (e) => {
         e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+            .then((authUser) => {})
+            .catch((error) => {
+                alert(error.message);
+            });
     };
 
     const signIn = (e) => {
         e.preventDefault();
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+            .then((authUser) => {})
+            .catch((error) => {
+                alert(error.message);
+            });
     };
 
     return (
         <div className="singupScreen">
             <form className="signupScreen_form">
                 <h1>Sign In</h1>
-                <input placeholder="Email" type="email" />
-                <input placeholder="Password" type="password" />
+                <input ref={emailRef} placeholder="Email" type="email" />
+                <input
+                    ref={passwordRef}
+                    placeholder="Password"
+                    type="password"
+                />
                 <button
                     className="signupScreen_button"
                     type="submit"

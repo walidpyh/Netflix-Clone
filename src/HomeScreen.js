18627e7d29f4d515a0f4a66c001f50de1537db35
Pyhoma69 <Pyhoma69@protonmail.com>
2022-01-28 17:10:32 -0800

    Fetching and adding movies to home

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,34 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
             <Nav />
             <Banner />
+
+            <Row
+                title="Only on Netflix"
+                fetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+            <Row
+                title="TV Action & Adventure"
+                fetchUrl={requests.fetchActionMovies}
+            />
+            <Row title="Comedies" fetchUrl={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+            <Row
+                title="Romance Movies"
+                fetchUrl={requests.fetchRomanceMovies}
+            />
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
         </div>
     );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -20,6 +20,15 @@ function Nav() {
         <div className={`nav ${show && "nav_black"}`}>
             <div className="nav_contents">
                 <img src="/assets/images/logo.png" className="nav_logo" />
+
+                <ul className="nav_menu">
+                    <li className="nav_menu_selected">Home</li>
+                    <li>TV Shows</li>
+                    <li>Movies</li>
+                    <li>New & Popular</li>
+                    <li>My List</li>
+                </ul>
+
                 <img src="/assets/images/profile.png" className="nav_avatar" />
             </div>
         </div>

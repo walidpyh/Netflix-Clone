b2e42844c91cbe874870d1fd538ea3a33b1482c0
Pyhoma69 <Pyhoma69@protonmail.com>
2022-01-29 08:52:00 -0800

    Linked redux to the userVar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((userAuth) => {
             if (userAuth) {
                 //logged
+                dispatch(
+                    login({
+                        uid: userAuth.uid,
+                        email: userAuth.email,
+                    })
+                );
             } else {
                 //logged out
+                dispatch(logout);
             }
         });
         return unsubscribe;
@@ -20,11 +32,16 @@ function App() {
     return (
         <div className="app">
             <BrowserRouter>
-                <Routes>
-                    <Route exact path="/" element={<HomeScreen />}></Route>
-                    <Route path="/YourAccount" element={<HomeScreen />}></Route>
-                    <Route path="/1" element={<LoginScreen />}></Route>
-                </Routes>
+                {!user ? (
+                    <LoginScreen />
+                ) : (
+                    <Routes>
+                        <Route exact path="/" element={<HomeScreen />}></Route>
+                    </Routes>
+                )}
+
+                {/*  <Route path="/YourAccount" element={<HomeScreen />}></Route>
+                    <Route path="/1" element={<LoginScreen />}></Route> */}
             </BrowserRouter>
         </div>
     );
